7964dabfb4f6c84b65db1f0f40ab6e19ad75e1a8
Abdulha Idrees <abdulhaidrees@pop-os.localdomain>
2021-04-14 12:13:27 +0200

    FitchaAbdulha

---
+++ javascript/product.js
@@ -0,0 +1,23 @@
+function infoGenerator() {
+    let newProduct = ""
+    newProduct =+ `
+    <section class="container_product">
+    <div class="product_description">
+      <section>
+        <button class="button_close">X</button>
+        <img class="photo_product" src="/_107435681_perro1.jpg" />
+        <aside class="info">
+          <h3 class="product_name">Product Name</h3>
+          <p class="reviews">⭐️⭐️⭐️⭐️12377 Reviews</p>
+          <p class="price">$ 180</p>
+          <input class="number_product" type="number" placeholder="0"/>
+          <button class="button_addCart">Add to cart</button>
+        </aside>
+      </section>
+      <section class="description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, consequatur dolorum quo explicabo fugit quia architecto voluptatibus expedita eius nisi quidem harum repellat veniam tempora? Vitae commodi architecto optio quod!</p>
+      </section>
+    </div>
+  </section>
+    `
+}
