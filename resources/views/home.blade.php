e16ea153ad92da191a5f73298eabaa3555ed8a6e
MAlexGG <alexandragalarzag@gmail.com>
2021-07-07 13:16:26 +0200

    creation of ProductController with CRUD, index done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+        @foreach ($products as $item)  
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="{{$item->image}}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">id.{{$item->id}} {{$item->title}}</h5>
+                <p class="card-text">{{$item->description}}</p>
+                <a href="#" class="btn btn-primary">Klikcoins: {{$item->klikcoinsProducts}}</a>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
