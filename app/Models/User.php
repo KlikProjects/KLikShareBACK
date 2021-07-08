39c5a6a18c293928a8b5ffe96d909e0f440f4cf2
MAlexGG <alexandragalarzag@gmail.com>
2021-07-08 13:50:40 +0200

    creation of user factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'image',
+        'klikcoinsUser',
+        'is_Admin',
     ];
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,5 +17,33 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         Product::factory(10)->create();
+
+
+        User::factory()->create([
+            'name' => 'root',
+            'email' => 'root@mail.com',
+            'is_Admin' => true
+        ]);
+        User::factory()->create([
+            'name' => 'vendedor1',
+            'email' => 'vendedor1@mail.com',
+            'is_Admin' => false
+        ]);
+        User::factory()->create([
+            'name' => 'vendedor2',
+            'email' => 'vendedor2@mail.com',
+            'is_Admin' => false
+        ]);
+        User::factory()->create([
+            'name' => 'comprador1',
+            'email' => 'comprador1@mail.com',
+            'is_Admin' => false
+        ]);
+        User::factory()->create([
+            'name' => 'comprador2',
+            'email' => 'comprador2@mail.com',
+            'is_Admin' => false
+        ]);
+
     }
 }
