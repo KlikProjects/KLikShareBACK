f6360ec008b782f9d77013acf4d5b61c8e16d798
Davidsd16 <davidsanchezdiaz72@gmail.com>
2021-07-27 12:30:55 +0200

    test del chat

--- tests/Feature/Api/ContactTest.php
+++ tests/Feature/Api/ContactTest.php
@@ -11,7 +11,7 @@ class ContactTest extends TestCase
     
     public function test_can_retreive_contacts()
     {
-        $response = $this->get('/');
+        $response = $this->get('/chatContacts');
 
         $response->assertStatus(200);
     }
