c0b1bdcd1b882e09ad045bd780620165e02e85ec
Armun4 <82039854+Armun4@users.noreply.github.com>
2021-07-27 14:16:20 +0200

    aa

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::get('/user', UserController::class);
 
 Route::get('/products',[ProductController::class,'index']);
 Route::get('/products/{id}',[ProductController::class,'show']);
-Route::post('/products', [ProductController::class, 'store'])/* ->middleware(['auth:api']) */;
+Route::post('/products', [ProductController::class, 'store'])->middleware(['auth:api']);
 Route::put('/products/{id}',[ProductController::class,'update'])->middleware(['auth:api']);
 Route::delete('/products/{id}',[ProductController::class,'destroy'])->middleware(['auth:api']);
 Route::get('/products/{id}/request', [ProductController::class, 'request'])->middleware(['auth:api']);
